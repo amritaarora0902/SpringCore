3cccd2abf926131f8e95c9c5543b23825aca5a16
amrita0902 <amritarora2424@gmail.com>
2021-05-11 12:28:22 +0530

    List Injection

---
+++ src/main/java/com/amrita/spring/springcore/list/Test.java
@@ -0,0 +1,14 @@
+package com.amrita.spring.springcore.list;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/list/listconfig.xml");
+		Hospital hospital = (Hospital) context.getBean("hospital");
+		System.out.println(hospital.getName());
+		System.out.println(hospital.getDepartments());
+	}
+
+}
