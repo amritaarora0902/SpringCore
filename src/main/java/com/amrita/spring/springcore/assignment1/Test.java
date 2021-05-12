095ccb60950f6ad41b60c598ee03a63a141b1769
amritaarora0902 <amritarora2424@gmail.com>
2021-05-12 17:44:23 +0530

    SetterInjection

--- src/main/java/com/amrita/spring/springcore/assignment1/Test.java
+++ src/main/java/com/amrita/spring/springcore/assignment1/Test.java
@@ -6,8 +6,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/assignment1/config.xml");
-		ShoppingCart sh = (ShoppingCart) con.getBean("shoppingcart");
-		System.out.println(sh);
+		ShoppingCart shop = (ShoppingCart) con.getBean("shoppingcart");
+		System.out.println(shop);
 		
 	}
 
