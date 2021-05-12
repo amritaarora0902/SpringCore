c7e87639a445132bc1c2a3c84e9845149743093d
amritaarora0902 <amritarora2424@gmail.com>
2021-05-12 17:10:34 +0530

    Setter-Injection

--- src/main/java/com/amrita/spring/springcore/assignment1/Test.java
+++ src/main/java/com/amrita/spring/springcore/assignment1/Test.java
@@ -5,8 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/assignment1/config.xml");
-		ShoppingCart sh = (ShoppingCart) context.getBean("shoppingcart");
+		ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/assignment1/config.xml");
+		ShoppingCart sh = (ShoppingCart) con.getBean("shoppingcart");
 		System.out.println(sh);
 		
 	}
