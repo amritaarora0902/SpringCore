55e8389dcb90920eec6e994dbf0c566f93e05046
amritaarora0902 <amritaarora2424@gmail.com>
2021-05-13 22:13:47 +0530

    pre-shutdown hook

--- src/main/java/com/amrita/spring/springcore/lc/xmlconfig/Test.java
+++ src/main/java/com/amrita/spring/springcore/lc/xmlconfig/Test.java
@@ -1,14 +1,15 @@
 package com.amrita.spring.springcore.lc.xmlconfig;
 
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/lc/xmlconfig/config.xml");
+		AbstractApplicationContext con = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/lc/xmlconfig/config.xml");
 		Patient patient = (Patient) con.getBean("patient");
 		System.out.println(patient);
-		
+		con.registerShutdownHook();
 	}
 
 }
