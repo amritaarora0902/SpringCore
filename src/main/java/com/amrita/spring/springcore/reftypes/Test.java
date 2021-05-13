0c66dfe0b82175293019153d7175467d58eec13f
amritaarora0902 <amritaarora2424@gmail.com>
2021-05-13 15:18:57 +0530

    SetterInject

--- src/main/java/com/amrita/spring/springcore/reftypes/Test.java
+++ src/main/java/com/amrita/spring/springcore/reftypes/Test.java
@@ -5,8 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/reftypes/config.xml");
-		Student student = (Student) context.getBean("student");
+		ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/reftypes/config.xml");
+		Student student = (Student) con.getBean("student");
 		System.out.println(student);
 		
 	}
