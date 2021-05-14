0839b67c6e0383a1092305613292ec2bcc5c36ac
amritaarora0902 <amritaarora2424@gmail.com>
2021-05-14 12:33:54 +0530

    Scopes

--- src/main/java/com/amrita/spring/springcore/innerbeans/Test.java
+++ src/main/java/com/amrita/spring/springcore/innerbeans/Test.java
@@ -8,7 +8,11 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/innerbeans/config.xml");
 		Employee emp = (Employee) context.getBean("employee");
-		System.out.println(emp);
+		System.out.println(emp.hashCode());
+		
+		Employee emp1 = (Employee) context.getBean("employee");
+		System.out.println(emp1.hashCode());
 	}
+	
 
 }
