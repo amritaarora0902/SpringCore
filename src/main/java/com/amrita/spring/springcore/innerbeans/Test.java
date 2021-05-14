c3d18ac2b9141839f39f79775c93f7a8acb9c116
amritaarora0902 <amritaarora2424@gmail.com>
2021-05-14 12:10:37 +0530

    Inner beans

---
+++ src/main/java/com/amrita/spring/springcore/innerbeans/Test.java
@@ -0,0 +1,14 @@
+package com.amrita.spring.springcore.innerbeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/innerbeans/config.xml");
+		Employee emp = (Employee) context.getBean("employee");
+		System.out.println(emp);
+	}
+
+}
