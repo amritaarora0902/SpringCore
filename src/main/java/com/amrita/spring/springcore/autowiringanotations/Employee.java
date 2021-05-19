e8574c4227d6939c741695e9f19e622fd1925f35
amritaarora0902 <amritaarora2424@gmail.com>
2021-05-19 15:05:45 +0530

    Autowired annotation

---
+++ src/main/java/com/amrita/spring/springcore/autowiringanotations/Employee.java
@@ -0,0 +1,26 @@
+package com.amrita.spring.springcore.autowiringanotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+
+	Employee(Address address){
+		this.address = address;
+	}
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+	
+	@Autowired
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+
+}
---
+++ src/main/java/com/amrita/spring/springcore/autowiringanotations/Test.java
@@ -0,0 +1,19 @@
+package com.amrita.spring.springcore.autowiringanotations;
+
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext con = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/autowiringanotations/config.xml");
+		Employee e = (Employee) con.getBean("employee");
+		System.out.println(e);
+		
+		
+	}
+
+}
