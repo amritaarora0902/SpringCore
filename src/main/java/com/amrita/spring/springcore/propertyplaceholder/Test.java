b530a2664924eaafaeee40bfa3ad1e6107977033
amritaarora0902 <amritaarora2424@gmail.com>
2021-05-19 11:12:51 +0530

    Bean Externalization

---
+++ src/main/java/com/amrita/spring/springcore/propertyplaceholder/Test.java
@@ -0,0 +1,12 @@
+package com.amrita.spring.springcore.propertyplaceholder;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+	ApplicationContext context = new ClassPathXmlApplicationContext("com/amrita/spring/springcore/propertyplaceholder/config.xml");
+	MyDAO dao = (MyDAO) context.getBean("myDAO");
+	System.out.println(dao);
+	}
+}	
